3d17266060ae60fb5e70ce40296d5ae89ed05dec
drabo@hp <dboneanu@gmail.com>
2020-10-10 14:24:40 +0300

    moved

--- alpine/build-and-push.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker rmi drabo/netchk-alpine
-docker build -t netchk-alpine:drabo .
-docker tag netchk-alpine:drabo drabo/netchk-alpine
-docker push drabo/netchk-alpine
--- debian-buster/build-and-push.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker rmi drabo/netchk-debian-buster
-docker build -t netchk-debian-buster:drabo .
-docker tag netchk-debian-buster:drabo drabo/netchk-debian-buster
-docker push drabo/netchk-debian-buster
