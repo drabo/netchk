09eed831e53aef1896d5e84a3d66b677effc5e98
drabo@hp <dboneanu@gmail.com>
2020-10-10 13:08:09 +0300

    optimize cleanup

--- debian-buster/run.sh
+++ debian-buster/run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-echo -e "\n\e[1;31mPull images\e[0m\n---"
-docker-compose pull
+if [[ "$1" != "clean" && "$1" != "delete" ]]; then
+    echo -e "\n\e[1;31mPull images\e[0m\n---"
+    docker-compose pull
+fi
 
 echo -e "\n\e[1;31mStop containers\e[0m\n---"
 docker-compose stop
