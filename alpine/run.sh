9cef4cec0a2156fafd45203e055cb5793e4f10dd
drabo@hp <dboneanu@gmail.com>
2021-01-06 14:04:50 +0200

    python version as parameter

--- alpine/run.sh
+++ alpine/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source ../version.rc
+
 if [[ "$1" != "clean" && "$1" != "delete" ]]; then
     echo -e "\n\e[1;31mPull images\e[0m\n---"
     docker-compose pull
@@ -12,6 +14,9 @@ echo -e "\n\e[1;31mDelete containers\e[0m\n---"
 docker-compose rm -v -f
 
 if [[ "$1" != "clean" && "$1" != "delete" ]]; then
-    echo -e "\n\e[1;31mBuild and run new containers\e[0m\n---"
-    docker-compose up --build -d
+    echo -e "\n\e[1;31mBuild new containers\e[0m\n---"
+    docker-compose build --build-arg PY_VER=${PY_VER}
+
+    echo -e "\n\e[1;31mRun new containers\e[0m\n---"
+    docker-compose up -d
 fi
