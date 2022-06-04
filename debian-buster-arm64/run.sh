db1b4297ad8353350449ca75b1f3305d1280411a
drabo@hp <dboneanu@gmail.com>
2022-06-04 14:50:26 +0300

    init

---
+++ debian-buster-arm64/run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source ../version.rc
+
+if [[ "$1" != "clean" && "$1" != "delete" ]]; then
+    echo -e "\n\e[1;31mPull images\e[0m\n---"
+    docker-compose pull
+fi
+
+echo -e "\n\e[1;31mStop containers\e[0m\n---"
+docker-compose stop
+
+echo -e "\n\e[1;31mDelete containers\e[0m\n---"
+docker-compose rm -v -f
+
+if [[ "$1" != "clean" && "$1" != "delete" ]]; then
+    echo -e "\n\e[1;31mBuild new containers\e[0m\n---"
+    docker-compose build --build-arg PY_VER=${PY_VER}
+
+    echo -e "\n\e[1;31mRun new containers\e[0m\n---"
+    docker-compose up -d
+fi
